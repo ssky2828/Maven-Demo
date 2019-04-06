c5bfdf79497fa149ee55cf3ba99ef5ad93487eec
Kumar <ratnakumarbt@gmail.com>
2019-04-06 16:36:07 -0300

    pom file added with the jar and assembly plugins

--- src/main/java/phani/App.java
+++ src/main/java/phani/App.java
@@ -1,5 +1,7 @@
 package phani;
 
+import org.apache.commons.lang3.StringUtils;
+
 /**
  * Hello world!
  *
@@ -8,6 +10,7 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        String str = "Phani";
+        System.out.println( "Hello World! -- " + StringUtils.reverse(str) );
     }
 }
